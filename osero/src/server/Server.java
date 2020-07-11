56e97884b9a57b9cdb5c5b7e4387b9411790a236
RyoSogabe999 <ryo52-sogabe@i.softbank.jp>
2020-07-11 18:20:26 +0900

    update sever -management method -register -update_record -csv directory

--- osero/src/server/Server.java
+++ osero/src/server/Server.java
@@ -15,6 +15,11 @@ public class Server {
         }
     };
 
+    static String name_p1, name_p2, name_p3, name_p4, name_p5, name_p6, name_p7, name_p8;
+
+    static String name_r1_1, name_r1_2, name_r2_1, name_r2_2, name_r3_1, name_r3_2, name_r4_1, name_r4_2;
+
+    static int d_p_1=10510, d_p_2=10520, d_p_3=10530, d_p_4=10540, d_p_5=10550, d_p_6=10560, d_p_7=10570, d_p_8=10580;
 
 
     public static void connect(){
@@ -65,8 +70,6 @@ public class Server {
 
 
     }
-
-
 
     public static boolean login(transData obj, ObjectOutputStream os){
         //ファイル読み込みで使用する３つのクラス
@@ -158,10 +161,329 @@ public class Server {
         }
     }
 
+    public static void update_record(transData obj){
+        FileInputStream fi = null;
+        InputStreamReader is = null;
+        BufferedReader br = null;
 
-    public static void update_record(){
+        FileInputStream fi_2 = null;
+        InputStreamReader is_2 = null;
+        BufferedReader br_2 = null;
+
+        try{
+            fi = new FileInputStream("csv/record.csv");
+            is = new InputStreamReader(fi);
+            br = new BufferedReader(is);
+
+            FileWriter fw = new FileWriter("csv/record_tmp.csv");
+            PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
+
+
+            if (obj.get_draw_flag()){
+                // todo when draw
+                String line;
+                String[] arr = null;
+                int i = 0;
+                while ((line = br.readLine()) != null){
+                    if (i == 0){
+                        pw.println(line);
+                        //                    System.out.println("line 1");
+                    }else{
+                        arr = line.split(",");
+                        //                    System.out.println(arr[0]);
+
+                        if(arr[0].equals(obj.get_winner()) || arr[0].equals(obj.get_loser())){
+                            int draw = Integer.parseInt(arr[3])+1;
+
+                            pw.print(arr[0]+",");
+                            pw.print(arr[1]+",");
+                            pw.print(arr[2]+",");
+                            pw.print(String.valueOf(draw)+",");
+                            pw.print(arr[4]);
+                            pw.println();
+                        }else{
+                            pw.println(line);
+                        }
+                    }
+                    i++;
+                }
+            }else{
+                // todo when not draw
+
+                String line;
+                String[] arr = null;
+                int i = 0;
+                while ((line = br.readLine()) != null){
+                    if (i == 0){
+                        pw.println(line);
+                        //                    System.out.println("line 1");
+                    }else{
+                        arr = line.split(",");
+                        //                    System.out.println(arr[0]);
+
+                        if(arr[0].equals(obj.get_winner())){
+                            // winner
+                            int win = Integer.parseInt(arr[1])+1;
+                            int rate = win*10 - Integer.parseInt(arr[2])*5;
+                            if(rate<0) rate = 0;
+
+                            pw.print(arr[0]+",");
+                            pw.print(String.valueOf(win)+",");
+                            pw.print(arr[2]+",");
+                            pw.print(arr[3]+",");
+                            pw.print(String.valueOf(rate)+",");
+                            pw.println();
+                        }else if(arr[0].equals(obj.get_loser())){
+                            // loser
+                            int lose = Integer.parseInt(arr[2])+1;
+                            int rate = Integer.parseInt(arr[1])*10 - lose*5;
+                            if(rate<0) rate = 0;
+
+                            pw.print(arr[0]+",");
+                            pw.print(arr[1]+",");
+                            pw.print(String.valueOf(lose)+",");
+                            pw.print(arr[3]+",");
+                            pw.print(String.valueOf(rate));
+                            pw.println();
+                        }else{
+                            pw.println(line);
+                        }
+                    }
+                    i++;
+                }
+            }
+
+
+            pw.close();
+            br.close();
+
+            // 書き換えたテンプファイルで上書き
+            fi_2 = new FileInputStream("csv/record_tmp.csv");
+            is_2 = new InputStreamReader(fi_2);
+            br_2 = new BufferedReader(is_2);
+
+            FileWriter fw_2 = new FileWriter("csv/record.csv");
+            PrintWriter pw_2 = new PrintWriter(new BufferedWriter(fw_2));
+
+            String line_2;
+            while ((line_2 = br_2.readLine()) != null) {
+                pw_2.println(line_2);
+            }
+            pw_2.close();
+            br_2.close();
+
+        }catch (IOException ex) {
+            ex.printStackTrace();
+        }
 
     }
+
+    public static boolean change_pass(transData obj, ObjectOutputStream oos){
+        //ファイル読み込みで使用する３つのクラス
+        FileInputStream fi = null;
+        InputStreamReader is = null;
+        BufferedReader br = null;
+
+        FileInputStream fi_2 = null;
+        InputStreamReader is_2 = null;
+        BufferedReader br_2 = null;
+
+        String ans = null;
+
+        try{
+            //読み込みファイルのインスタンス生成
+            //ファイル名を指定する
+            fi = new FileInputStream("csv/login.csv");
+            is = new InputStreamReader(fi);
+            br = new BufferedReader(is);
+
+            FileWriter fw = new FileWriter("csv/login_tmp.csv");
+            PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
+
+            //読み込み行
+            String line;
+
+            //列名を管理する為の配列
+            String[] arr = null;
+
+            //1行ずつ読み込みを行う
+            //1行目から検索して名前が一致したらパスを確認
+            //一致したら "true" 違えば "false password" を返す
+            int i = 0;
+            while ((line = br.readLine()) != null){
+                if (i == 0){
+                    pw.println(line);
+//                    System.out.println("line 1");
+                }else{
+                    arr = line.split(",");
+//                    System.out.println(arr[0]);
+
+                    if(arr[0].equals(obj.get_change_name())){
+                        if (arr[2].equals(obj.get_change_secret_question())){
+                            // todo 認証成功　パスワード変更
+                            ans = "change succeed";
+
+                            pw.print(arr[0]+",");
+                            pw.print(obj.get_change_pass()+",");
+                            pw.print(arr[2]+",");
+                            pw.println();
+
+//                            break;
+                        }else{
+                            ans = "change failed : false question answer";
+                            pw.println(line);
+//                            break;
+                        }
+                    }else{
+                        pw.println(line);
+                    }
+                }
+                i++;
+            }
+            if(ans==null){
+                ans = "change failed : this name does not exist";
+            }
+
+            pw.close();
+            br.close();
+
+            // 書き換えたテンプファイルで上書き
+            fi_2 = new FileInputStream("csv/login_tmp.csv");
+            is_2 = new InputStreamReader(fi_2);
+            br_2 = new BufferedReader(is_2);
+
+            FileWriter fw_2 = new FileWriter("csv/login.csv");
+            PrintWriter pw_2 = new PrintWriter(new BufferedWriter(fw_2));
+
+            String line_2;
+            while ((line_2 = br_2.readLine()) != null) {
+                pw_2.println(line_2);
+            }
+            pw_2.close();
+            br_2.close();
+
+
+            // 送信用インスタンス作成
+            transData change_ans = new transData(11);
+            change_ans.set_login_answer(ans);
+
+
+            //send
+            oos.writeObject (change_ans);
+//            oos.flush();
+
+            System.out.println(change_ans.get_login_answer());
+            System.out.println("change done");
+
+
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+
+        }
+
+        if (ans.equals("change succeed")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean register(transData obj){
+        FileInputStream fi = null;
+        InputStreamReader is = null;
+        BufferedReader br = null;
+
+        FileInputStream fi_2 = null;
+        InputStreamReader is_2 = null;
+        BufferedReader br_2 = null;
+
+        String ans=null;
+        try {
+
+            //同じ名前のユーザがいないか判定
+            boolean ok_flag = true;
+            fi = new FileInputStream("csv/login.csv");
+            is = new InputStreamReader(fi);
+            br = new BufferedReader(is);
+            String line;
+            String[] arr = null;
+            int i = 0;
+            while ((line = br.readLine()) != null){
+                if (i == 0){
+//                    System.out.println("line 1");
+                }else{
+                    arr = line.split(",");
+//                    System.out.println(arr[0]);
+
+                    if(arr[0].equals(obj.get_register_name())){
+                        ok_flag = false;
+                    }
+                }
+                i++;
+            }
+
+
+            if(ok_flag==true){
+                FileWriter f = new FileWriter("csv/login.csv", true);
+                PrintWriter p = new PrintWriter(new BufferedWriter(f));
+
+                p.print(obj.get_register_name());
+                p.print(",");
+                p.print(obj.get_register_pass());
+                p.print(",");
+                p.print(obj.get_register_secret_question());
+                p.println();
+
+                p.close();
+
+
+                //  record
+                FileWriter f_2 = new FileWriter("csv/record.csv", true);
+                PrintWriter p_2 = new PrintWriter(new BufferedWriter(f_2));
+
+                p_2.print(obj.get_register_name());
+                p_2.print(",0,0,0,0");
+
+                p_2.println();
+
+                p_2.close();
+
+                ans = "register succeed";
+                System.out.println(ans);
+
+            }else {
+
+                ans = "Register failed : this name is already used";
+                System.out.println(ans);
+            }
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }finally{
+            try {
+                br.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if (ans.equals("register succeed")) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    public static void manage(){
+        Manage m = new Manage();
+
+        m.start();
+
+    }
+
+
 
     public static void main(String[] args){
         System.out.println("main start--------------------");
@@ -170,6 +492,8 @@ public class Server {
         connect();
 
         room();
+
+        manage();
 
 
         System.out.println("--------------------main close");
