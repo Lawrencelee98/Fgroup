2f0cad85c2a48be5a41c282781a17c840b5559e9
KHANGMINJE <56621402+KHANGMINJE@users.noreply.github.com>
2020-08-02 20:42:35 +0900

    Update Result.java

--- osero/src/client/Result.java
+++ osero/src/client/Result.java
@@ -1,59 +1,93 @@
 package client;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-import javax.swing.*;
-import transData.*;
-public class Result extends JFrame {
-    JLabel l = new JLabel("");
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+public class Result extends JFrame implements ActionListener{
+	JLabel l = new JLabel("");
+    JButton b= new JButton("New Game");
     Client client = null;
     int result;
-    int display;
-    JFrame j;
-    public Result(int result, Client client,int display,JFrame j) {
+    Map<Integer, Integer> room_info;
+    List<String> players_info;
+    
+    public Result(int result, Client client,Map<Integer, Integer> room_info, List<String> players_info)  {
         this.client = client;
         this.result = result;
-        this.j = j;
-        System.out.println("Result : "+ result+ "display:"+display);
-        l.setBounds(70, 50, 50, 50);
+        this.room_info=room_info;
+        this.players_info=players_info;
+        
+        System.out.println("Result : "+ result);
+        l.setBounds(70, 20, 50, 50);
         add(l);
         if(result==0){
-            l.setText("白の勝ち");
-        }else if(result ==1){
             l.setText("黒の勝ち");
+        }else if(result ==1){
+            l.setText("白の勝ち");
         }else if(result ==2){
             l.setText("引き分け");
         }
-       
-       
+
+        b.setBounds(30, 80, 120, 50);
+        b.addActionListener(this);
+        add(b);
+
         this.setLayout(null);
         this.setSize(200, 200);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
-    
-        try {
-            j.dispose();
-            // client.s.close();
-             client.setLoginPort(client.FirstConnect(client.ServerAddress, client.first_port));
-             Socket s = new Socket(client.ServerAddress,client.getLoginPort());
-             client.s = s;
-             new Login_display("login",client);
-             OutputStream os = s.getOutputStream();
-             ObjectOutputStream oos = new ObjectOutputStream(os);
-             InputStream is = s.getInputStream();
-             ObjectInputStream ois = new ObjectInputStream(is);
-             client.choose_room(oos, ois);
-           
-         } catch (Exception e) {
-             // TODO 自動生成された catch ブロック
-             e.printStackTrace();
-         }
-    
+
+
 
     }
-    
+    public void actionPerformed(ActionEvent e){
+    	if (e.getSource() == b) {
+    		/*try {
+                 client.s.close();
+                 client.setLoginPort(client.FirstConnect(client.ServerAddress, client.first_port));
+                 Socket s = new Socket(client.ServerAddress,client.getLoginPort());
+                 client.s = s;
+                 OutputStream os = s.getOutputStream();
+                 ObjectOutputStream oos = new ObjectOutputStream(os);
+                 InputStream is = s.getInputStream();
+                 ObjectInputStream ois = new ObjectInputStream(is);
+                 client.choose_room(oos, ois);
+                 System.out.println("open new room");
+     			 this.setVisible(false);
+             } catch (IOException es) {
+                 // TODO 自動生成された catch ブロック
+                 es.printStackTrace();
+             }
+    		*/
+    		room_info.put(1, 0);
+    		new Room(client, room_info, players_info, client.oos, client.ois);
+    		/*try {
+    			System.out.println("roomData");
+    			transData t=(transData)client.ois.readObject();
+    			if(t.get_protocol()==12) {
+    			Map<Integer, Integer> room_info = t.get_room_info();
+    			System.out.println(room_info);
+    			List<String> players_info = t.get_players_info();
+    			System.out.println(players_info);
+    			client.choose_room(client.oos, client.ois);
+    			System.out.println("open new room");
+    			}
+    		} catch (Exception ee) {
+    			// ee.printStackTrace();
+    		}*/
+
+    		
+            this.dispose();
+
+
+
+
+		}
+    }
+
 }
