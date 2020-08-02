7abbe24917c57effb488786d976b4a1f1b5dfceb
KHANGMINJE <56621402+KHANGMINJE@users.noreply.github.com>
2020-08-02 22:00:09 +0900

    Update Osero_setting.java

--- osero/src/client/Osero_setting.java
+++ osero/src/client/Osero_setting.java
@@ -16,6 +16,10 @@ public class Osero_setting extends JFrame implements ActionListener{
 	JButton b4 = new JButton("金色");
 	JButton b5 = new JButton("赤色");
 
+    	JLabel l6 = new JLabel("置ける場所表現 : ");
+	JRadioButton r1 = new JRadioButton("0n", true);
+    	JRadioButton r2 = new JRadioButton("Off", false);
+
 	
 	JLabel l2 = new JLabel("盤面の色");
 	JFrame j = new JFrame();
@@ -30,6 +34,22 @@ public class Osero_setting extends JFrame implements ActionListener{
 	this.chessboard = chessboard;
 	b1.setBounds(10,10,80,30);
 	b1.addActionListener(this);
+		
+	
+    	l6.setBounds(150, 150, 100, 30);
+    	ButtonGroup group = new ButtonGroup();
+	group.add(r1);
+	group.add(r2);
+	r1.setBounds(250, 150, 50, 30);
+	r2.setBounds(300, 150, 50, 30);
+
+   	r1.addActionListener(this);
+    	r2.addActionListener(this);
+    
+    
+	b1.setBounds(10,10,80,30);
+	b1.addActionListener(this);
+
 
 	
 	
@@ -51,11 +71,26 @@ public class Osero_setting extends JFrame implements ActionListener{
 	c.add(b3);
 	c.add(b4);
 	c.add(b5);
+		
+	c.add(l6);
+    	c.add(r1);
+    	c.add(r2);
+
 
 	j.setVisible(true);
 	}
 
 	public void actionPerformed(ActionEvent ae) {
+		
+    		boolean status1 = r1.isSelected();
+		boolean status2 = r2.isSelected();
+	    	if (ae.getSource() == r1 || ae.getSource() == r2) {
+			if(status1 == true) {
+				Oserov4.G_switch = true;
+    			}else if(status2 == true) {
+    				Oserov4.G_switch = false;
+    			}
+		}
 		if(ae.getSource()==b1) {
 			System.out.println("close setting window");
 			//j.setVisible(false);
